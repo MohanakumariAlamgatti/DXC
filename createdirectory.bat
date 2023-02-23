4a112a3e58c7735c01d78abb7ae87691288b30ce
MohanakumariAlamgatti <126164006+MohanakumariAlamgatti@users.noreply.github.com>
2023-02-23 17:39:39 +0530

    create directory file

---
+++ createdirectory.bat
@@ -0,0 +1,32 @@
+cd c:\
+
+mkdir PowerShell
+
+cd PowerShell
+
+mkdir Mydir1
+
+mkdir Mydir2
+
+mkdir Mydir3
+
+cd Mydir1
+
+mkdir Mydir11
+
+mkdir Mydir12
+
+cd ..
+
+cd Mydir2
+
+mkdir Mydir21
+
+cd ..
+
+cd Mydir3
+
+mkdir Mydir31
+
+mkdir Mydir32
+
